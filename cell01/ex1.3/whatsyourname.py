e69b76d82450f4a360da395808ed991aca9f7d56
psaeung <Nepjune0561@gmail.com>
2025-06-17 04:12:57 +0000

    update

---
+++ cell01/ex1.3/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input("Enter your first name: ")
+Last_name = input("Enter your Last name: ")
+print(first_name,Last_name)
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+while True:
+    user_input = input("Say something (type 'STOP' to exit): ")
+    
+    if user_input.upper() == "STOP":
+        
