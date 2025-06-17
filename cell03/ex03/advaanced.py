9d262ed60d40456ff6a017f26c0f374ae29311e0
psaeung <Nepjune0561@gmail.com>
2025-06-17 05:07:20 +0000

    ff

--- cell03/ex03/advaanced.py
+++ cell03/ex03/advaanced.py
@@ -4,4 +4,10 @@ def print_multiplication_tables():
 
     for i in range(1, 11):
         print(f"Table for {i}:")
-        
+        for j in range(1, 11):
+            result = i * j
+            print(f"  {i} x {j} = {result}")
+
+        print("")
+
+print_multiplication_tables()
